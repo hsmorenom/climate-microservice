a9f64aaa22672e38cef0e1ca36e6b6c8d7912660
hsmorenom <sebas03272009@hotmail.com>
2026-01-30 20:33:45 -0500

    feat: new microservice oriented to climate data

---
+++ src/main/java/com/sebastianmoreno/climate_microservice/ClimateMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sebastianmoreno.climate_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateMicroserviceApplication.class, args);
+	}
+
+}
