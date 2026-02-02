ed5c2dd19eafecc3e2e328e571101b6a4d51f271
hsmorenom <sebas03272009@hotmail.com>
2026-02-02 17:27:54 -0500

    fix: Add new content about API Simulation from CLimate API

--- src/main/java/com/sebastianmoreno/climate_microservice/service/ClimateService.java
+++ src/main/java/com/sebastianmoreno/climate_microservice/service/ClimateService.java
@@ -3,11 +3,29 @@ package com.sebastianmoreno.climate_microservice.service;
 import com.sebastianmoreno.climate_microservice.model.ClimateResponseDTO;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class ClimateService {
-    public ClimateResponseDTO getStatus(){
-        return new ClimateResponseDTO(
-                "Climate Microservice", "Running", "v1.0"
+    public ClimateResponseDTO getClimateReport(String ciudad){
+        //Simulación de datos obtenidos de API
+        double precipitacion = 120.5;
+        double temperatura = 13.8;
+        double evapotranspiracion = 45.2;
+
+        String interpretacion;
+        //Simulacion de interpretación de datos
+        if(precipitacion>100){
+            interpretacion = "Alta precipitación: posible riesgo de saturación hídrica en suelos.";
+        } else{
+            interpretacion ="Precipitación moderada: condiciones estables para cultivos.";
+        }
+
+        //Construccion de modelo limpio de dto
+        ClimateResponseDTO dto=new ClimateResponseDTO(
+                "Estación El Dorado",ciudad,"Cundinamarca",precipitacion,temperatura,evapotranspiracion,
+                LocalDate.now(),interpretacion
         );
+        return dto;
     }
 }
