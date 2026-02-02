d8e247fc97f752a62adef8c96a7dca15d06a6996
hsmorenom <sebas03272009@hotmail.com>
2026-02-02 17:10:52 -0500

    feat: new content in DTO about putting constructors, getters and setters from real data of climate

--- src/main/java/com/sebastianmoreno/climate_microservice/model/ClimateResponseDTO.java
+++ src/main/java/com/sebastianmoreno/climate_microservice/model/ClimateResponseDTO.java
@@ -1,24 +1,98 @@
 package com.sebastianmoreno.climate_microservice.model;
 
+import java.time.LocalDate;
+
 public class ClimateResponseDTO {
-    private String name;
-    private String status;
-    private String version;
+    private String estacion;
+    private String ciudad;
+    private String departamento;
+    private double precipitacionMensualMm;
+    private double temperaturaMediaC;
+    private double evapotranspiracionMm;
+    private LocalDate fechaRegistro;
+    private String InterpretacionTecnica;
 
-    public ClimateResponseDTO(String name, String status, String version){
-        this.name=name;
-        this.status=status;
-        this.version=version;
+    //Constructor Vacio que lo necesita SpringBoot
+    public ClimateResponseDTO(){
+
     }
-    public String getName(){
-        return name;
-    }
-    public String getStatus(){
-        return status;
+    //Constructor de las variables
+    public ClimateResponseDTO(String estacion, String ciudad, String departamento, double precipitacionMensualMm,
+                              double temperaturaMediaC, double evapotranspiracionMm, LocalDate fechaRegistro,
+                              String interpretacionTecnica) {
+        this.estacion = estacion;
+        this.ciudad = ciudad;
+        this.departamento = departamento;
+        this.precipitacionMensualMm = precipitacionMensualMm;
+        this.temperaturaMediaC = temperaturaMediaC;
+        this.evapotranspiracionMm = evapotranspiracionMm;
+        this.fechaRegistro = fechaRegistro;
+        InterpretacionTecnica = interpretacionTecnica;
     }
 
-    public String getVersion(){
-        return version;
+    //Getters y Setters de las variables
+
+    public String getEstacion() {
+        return estacion;
     }
 
+    public void setEstacion(String estacion) {
+        this.estacion = estacion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public double getPrecipitacionMensualMm() {
+        return precipitacionMensualMm;
+    }
+
+    public void setPrecipitacionMensualMm(double precipitacionMensualMm) {
+        this.precipitacionMensualMm = precipitacionMensualMm;
+    }
+
+    public double getTemperaturaMediaC() {
+        return temperaturaMediaC;
+    }
+
+    public void setTemperaturaMediaC(double temperaturaMediaC) {
+        this.temperaturaMediaC = temperaturaMediaC;
+    }
+
+    public double getEvapotranspiracionMm() {
+        return evapotranspiracionMm;
+    }
+
+    public void setEvapotranspiracionMm(double evapotranspiracionMm) {
+        this.evapotranspiracionMm = evapotranspiracionMm;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getInterpretacionTecnica() {
+        return InterpretacionTecnica;
+    }
+
+    public void setInterpretacionTecnica(String interpretacionTecnica) {
+        InterpretacionTecnica = interpretacionTecnica;
+    }
 }
