326b04fbd55dea31da8ef6028117650e593f45f2
hsmorenom <sebas03272009@hotmail.com>
2026-02-03 18:58:52 -0500

    feat: add and create files to manage temperature API from datos.gov.co

---
+++ src/main/java/com/sebastianmoreno/climate_microservice/model/TemperaturaDTO.java
@@ -0,0 +1,4 @@
+package com.sebastianmoreno.climate_microservice.model;
+
+public class TemperaturaDTO {
+}
---
+++ src/main/java/com/sebastianmoreno/climate_microservice/service/TemperaturaService.java
@@ -0,0 +1,7 @@
+package com.sebastianmoreno.climate_microservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemperaturaService {
+}
