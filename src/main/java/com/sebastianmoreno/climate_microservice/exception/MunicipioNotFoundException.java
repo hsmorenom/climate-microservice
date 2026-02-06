54ecd099d9c21bd7c112be81680afb64dc00dbe2
hsmorenom <sebas03272009@hotmail.com>
2026-02-05 20:25:44 -0500

    feat: add new function in order to manage exceptions

---
+++ src/main/java/com/sebastianmoreno/climate_microservice/exception/MunicipioNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sebastianmoreno.climate_microservice.exception;
+
+public class MunicipioNotFoundException extends RuntimeException {
+
+    public MunicipioNotFoundException(String municipio){
+        super("Municipion no encontrado"+municipio);
+    }
+}
